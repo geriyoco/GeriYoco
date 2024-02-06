cc386387e94346c4a38b40eb7e53d11a98ab4337
Fanurs Teh <fyanurs@gmail.com>
2024-02-05 22:32:09 -0500

    baseurl should match repo name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://geriyoco.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/geriyoco/',
+  baseUrl: '/GeriYoco/',
   trailingSlash: true,
 
   // GitHub pages deployment config.
