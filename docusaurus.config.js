f966d3d0cb4fedc0e62dc780f0de17a5f887ca4d
Fanurs Teh <fyanurs@gmail.com>
2024-02-05 22:43:22 -0500

    Try using with trailingSlash false

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,15 +13,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://geriyoco.github.io/',
+  url: 'https://geriyoco.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/GeriYoco/',
-  trailingSlash: true,
+  baseUrl: '/',
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'GeriYoco', // Usually your GitHub org/user name.
+  organizationName: 'geriyoco', // Usually your GitHub org/user name.
   projectName: 'GeriYoco', // Usually your repo name.
 
   onBrokenLinks: 'throw',
